32f488e8fddf638119fce60cde644c4615ad2e54
adaKUUU <adahir320@gmail.com>
2023-02-19 22:29:03 -0600

    cambio ruta sw

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+var url = window.location.href;
+var swLocation  ='/pwatw/sw.js';
 
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+    if(navigator.serviceWorker) {
+        if(url.includes('localhost')){
+            swLocation = '/sw.js';
+        }
+        navigator.serviceWorker.register( swLocation);
 }
+    
+
 
 // Referencias de jQuery
 
